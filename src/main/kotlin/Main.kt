458a879bb3259139078c92f86be461986e75de29
SempaiN <trollnacho.np@gmail.com>
2023-09-17 12:18:03 +0200

    perrito

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,38 @@ fun main(args: Array<String>) {
  print("Dame un número y te digo el doble")
  val numero = readln().toInt();
  println("El doble de $numero es ${numero*2}")
- */
+
+print("Dame la temperatura en grados centígrados y lo paso a Fahernheit ")
+    var gradosC = readln().toInt();
+    var gradosF = 32+ (9*gradosC/5);
+    println("La temperatura original es $gradosC en fahrenheit es $gradosF")
+    */
+    /*
+print("Dame el radio de una circunferencia")
+    var radio = readln().toInt();
+    var longitud = 2*Math.PI*radio;
+    var area =Math.PI * (radio*radio);
+    println("la longitud es $longitud y el area es $area")
+*/
+    /*
+  print("Dame una velocidad en kilometros hora y lo pao a metros segundo ")
+    val velocidadK = readln().toDouble();
+    val velocidadM = velocidadK/3.6;
+    println("La velocidad en m/s es $velocidadM")*/
+/*
+    print("Dame la longitud del primer cateto")
+    val cateto1 = readln().toDouble()
+    print("\n Dame la longitud del segundo cateto")
+    val cateto2 = readln().toDouble();
+
+    var hipotenusa = (cateto1*cateto1)+(cateto2*cateto2)
+    hipotenusa = Math.sqrt(hipotenusa);
+    println("La hipotenusa es $hipotenusa")
+*/
+
+    println("Dame un radio para calcular el volumen de una esfera")
+    val radio = readln().toDouble();
+
 
 }
 
