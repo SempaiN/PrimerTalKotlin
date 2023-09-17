6f8406345657d73f88e77a860c274da9b084609a
SempaiN <trollnacho.np@gmail.com>
2023-09-17 11:43:44 +0200

    kk

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    /*var num1 = readln();
+    var num2 = readln();
+    println("El pimer número es $num1 el segundo es $num2")
+
+ println("dime un nombre")
+ val nombre:String = readln();
+ println("Buenos días $nombre")
+ */
+ /*
+ print("Dame un número y te digo el doble")
+ val numero = readln().toInt();
+ println("El doble de $numero es ${numero*2}")
+ */
+
+}
+
